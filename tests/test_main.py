971ac2bdf39e715a8214b7bb15450e7579d263e1
laith204 <laith.oraha04@gmail.com>
2025-04-13 23:07:03 -0700

    got tests working. going to add more

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
-from main import app
+from src.app.main import app
 
 client = TestClient(app)
 
 def test_get_user():
-    response = client.get("/user/1")
+    response = client.get("auth/user/1")
     assert response.status_code == 200

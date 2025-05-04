ecd68de8592ca705e9fc0f793e6a6b17a62ec54c
Logan Hall <ha11l0ga5@gmail.com>
2025-05-04 00:41:32 -0700

    added our frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      {"Hello Working Website"}
-    </div>
+    <main>
+      <h1>
+        Hello Website
+      </h1>
+    </main>
   );
 }
---
+++ frontend/src/components/PageSelect.tsx
@@ -0,0 +1,9 @@
+export default function PageSelect() {
+  return (
+    <main>
+      <h1>
+        PageSelect
+      </h1>
+    </main>
+  );
+}

2a04adb947b5b8f21c6bf298cda4c5362069a7d7
SatyaNampalli <nagarjun1704@gmail.com>
2025-05-04 19:44:03 -0700

    added forgot password

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -21,12 +21,15 @@ import { useAuth } from '@/context/AuthContext'
 import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
-    username: z.string().min(3, {
-        message: 'Username must be at least 3 characters.',
-    }),
-    password: z.string().min(6, {
-        message: 'Password must be at least 6 characters.',
-    }),
+  username: z.string().min(8, { message: "Username must be at least 8 characters" })
+  .max(40, { message: "Username must be at most 64 characters" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" })
+    .max(64, { message: "Password must be at most 64 characters" })
+    .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+    .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^a-zA-Z0-9]/, { message: "Password must contain at least one special character" }),
 });
 
 type FormValues = z.infer<typeof formSchema>;
